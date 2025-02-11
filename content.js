548c48e080466c2482174dcaa2341a06673342dc
= <=>
2025-02-11 16:24:22 +0900

    feat: Extract background color and text color by click
    - 클릭을 통하여 배경 색과 글자 색 추출

--- content.js
+++ content.js
@@ -2,9 +2,14 @@ document.addEventListener("click", (event) => {
   event.preventDefault(); //기본 클릭 동작 방지
 
   let element = event.target;
+  let elementStyle = window.getComputedStyle(element);
+
+  let textColor = elementStyle.color;
+  let backgroundColor = elementStyle.backgroundColor;
 
   console.log("클릭한 요소 정보");
   console.log("태그명: ", element.tagName);
   console.log("ID: ", element.id || "없음");
-  console.log("event: ", event);
+  console.log("textcolor: ", textColor || "없음");
+  console.log("bgColor: ", backgroundColor || "없음");
 });
