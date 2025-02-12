cb921461c947d83510e3c5c05c1710105ce9f9b7
= <=>
2025-02-12 16:10:47 +0900

    feat: Add right click menu when enter picking color mode
    - 색 추출 모드에 들어가기 위해 우클릭 메뉴 추가

--- background.js
+++ background.js
@@ -1,4 +1,21 @@
 // 확장 프로그램의 백그라운드 동작 구현
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Extension installed!");
+  chrome.contextMenus.create({
+    id: "pickColor",
+    title: "색 추출 모드",
+    contexts: ["all"],
+  });
 });
+
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if (info.menuItemId === "pickColor") {
+    chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      function: confirmConsole,
+    });
+  }
+});
+
+function confirmConsole() {
+  console.log("right click attached!");
+}
